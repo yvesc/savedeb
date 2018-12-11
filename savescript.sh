dff3ca8e5868c342e8d8889c9a14b6cd78bb258c
yvesc <boofhiggs@gmail.com>
2018-12-11 13:17:23 +0100

    Initial commit.

---
+++ savescript.sh
@@ -0,0 +1,12 @@
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/lib/dpkg/lock
+sudo rm /var/lib/dpkg/lock-frontend
+sudo dpkg --configure -a
+sudo apt clean
+sudo apt update --fix-missing
+sudo apt install -f
+sudo dpkg --configure -a
+sudo apt upgrade
+sudo apt dist-upgrade
+sudo reboot
+
